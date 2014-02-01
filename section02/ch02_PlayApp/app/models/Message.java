04bced9e069a0fdd44868ca0618b9b8598c8ba01
makopi23 <kipersonalpc@gmail.com>
2014-02-01 16:26:50 +0900

    section2.3.5 is completed.

--- section02/ch02_PlayApp/app/models/Message.java
+++ section02/ch02_PlayApp/app/models/Message.java
@@ -5,15 +5,24 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import com.avaje.ebean.annotation.CreatedTimestamp;
+
+import play.data.validation.Constraints.Required;
 import play.db.ebean.Model;
 
 @Entity
 public class Message extends Model {
 	@Id
 	public Long id;
+	
+	@Required
 	public String name;
 	public String mail;
+	
+	@Required
 	public String message;
+	
+	@CreatedTimestamp
 	public Date postdate;
 	
 	public static Finder<Long, Message> find =
